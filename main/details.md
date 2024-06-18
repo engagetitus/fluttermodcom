1d06a8611a624d967a8dd7d252068e5530c65fc4
Gadgeted <collinsmaina1921@gmail.com>
2024-06-18 09:59:30 +0300

    Changes

--- main/details.md
+++ main/details.md
@@ -1,4 +1,8 @@
 ## Main Course Project : School Management System
 
+# 18th June - create project
+packege name -> ke.co.modcom.flutter lms
+
+
 
 
--- side/messages/android/app/src/main/kotlin/ke/co/titus/modcom/messages/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package ke.co.titus.modcom.messages
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
