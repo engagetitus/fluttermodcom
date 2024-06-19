e6d6bdfdbe408668792dd73cb41e69d9fa3a2e89
Wamiti1 <davidwamiti11200@gmail.com>
2024-06-19 12:43:18 +0300

    labs

--- main/lms/lib/screens/signup.dart
+++ main/lms/lib/screens/signup.dart
@@ -1,186 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
+class Signup extends StatelessWidget {
+  const Signup({super.key});
 
-
-
-class SignUp extends StatefulWidget {
-  const SignUp({super.key});
-
-  @override
-  State<SignUp> createState() => _SignUpState();
-}
-
-class _SignUpState extends State<SignUp> {
-  
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('')
+    return  Scaffold(
+        appBar: AppBar(
+          title: const Text(''),
 
-      ),
+        ),
 
-      body:ListView(
-        scrollDirection: Axis.vertical,
-        reverse: true,
-        children: [
-          Column(
-              
-          
-              children:[
-                const Column(
-                  children:[
-                Text('Welcome', style: TextStyle(fontSize: 30.0),),
-                Text('Sign Up', style: TextStyle(fontSize: 23.0),),
-              ]),
-          
-          
-          
-              const Padding(
-                padding: EdgeInsets.all(15.0),
-                child: TextField(
-                    decoration: InputDecoration(
-                      labelText: 'FirstName',
-                      hintText : 'Mary',
-                      prefixIcon : Icon(Icons.person),
-                      border: OutlineInputBorder(),
-                    ),
-                    
-                  ),
-              ),
-          
-          
-                const Padding(
-                  padding: EdgeInsets.all(15.0),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      labelText: 'LastName',
-                      hintText : 'Kamau',
-                      prefixIcon : Icon(Icons.person),
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                ),
-          
-          
-          
-                const Padding(
-                  padding: EdgeInsets.all(15.0),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Email',
-                      hintText : 'example@gmail.com',
-                      prefixIcon : Icon(Icons.email),
-                      border: OutlineInputBorder(),
-                    ),
-                    keyboardType: TextInputType.emailAddress
-                  ),
-                ),
-          
-                const Padding(
-                  padding: EdgeInsets.all(15.0),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Phone',
-                      hintText : '254721465789',
-                      prefixIcon : Icon(Icons.phone),
-                      border: OutlineInputBorder(),
-                    ),
-                  
-                    keyboardType: TextInputType.phone
-                  ),
-                ),
-          
-                const Padding(
-                  padding: EdgeInsets.all(15.0),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Course',
-                      hintText : 'Bachelor of Science in Computer Science',
-                      prefixIcon : Icon(Icons.book),
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                ),
-          
-                const Padding(
-                  padding: EdgeInsets.all(15.0),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Lab',
-                      hintText : 'Lab 9',
-                      prefixIcon : Icon(Icons.cell_tower),
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                ),
-          
-                const Padding(
-                  padding: EdgeInsets.all(15.0),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Profile Image',
-                      hintText : 'url',
-                      prefixIcon : Icon(Icons.link),
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                ),
-          
-                const Padding(
-                  padding: EdgeInsets.all(15.0),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Github',
-                      hintText : 'url',
-                      prefixIcon : Icon(Icons.link),
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                ),
-          
-                const Padding(
-                  padding: EdgeInsets.all(15.0),
-                  child: TextField(
-                    decoration: InputDecoration(
-                      labelText: 'Address',
-                      hintText : 'P.O. BOX 45763 Muranga',
-                      prefixIcon : Icon(Icons.home_filled),
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                ),
-          
-                TextButton(
-                style: ButtonStyle(
-                foregroundColor: WidgetStateProperty.all<Color>(Colors.blue),
-                      ),
-                onPressed: () {
-          
-          
-                },
-                child: const Text('Sign up'),
-          )
-          
-          
-              ]
-          
-              ),
+
+
+      body: const Column(
+        children: [Column(
+          children: [
+            Text('Welcome', textAlign: TextAlign.center, style: TextStyle(fontSize: 50.9),
+            
+            ),
+        
+            Text('Login to your account ', textAlign: TextAlign.center, style: TextStyle(fontSize: 15.2),
+            
+            )
+          ],
+        ),
+        
+            Column(children: [Padding(
+              padding: EdgeInsets.all(30.0),
+              child: TextField(readOnly: false, showCursor: true, autocorrect: false, keyboardType: TextInputType.emailAddress, decoration: InputDecoration(hintText: 'Enter your email address'),),
+            ), 
+            
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 13.0),
+              child: TextField(readOnly: false, showCursor: true, autocorrect: false, keyboardType: TextInputType.numberWithOptions(signed: true, decimal: true), decoration: InputDecoration(hintText: 'Enter your password'),),
+            )]),
+        
         ],
       ),
-          
-
-            );
-          
-      
-    
+    );
   }
-
-
-      
-      
-    
-
-
-
 }
