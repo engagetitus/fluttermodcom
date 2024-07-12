fc34525187d09c2d276cca8c6a9d67f2140c1003
Wamiti1 <davidwamiti11200@gmail.com>
2024-07-12 23:03:07 +0300

    moved to another repository(soko_garden)

--- side/soko_garden/README.md
+++
@@ -1,4 +0,0 @@
-# soko_garden
-
-- API endpoint = https://soko.titus.co.ke/api/products
-- https://soko.titus.co.ke/api/mpesa
--- side/soko_garden/android/app/src/main/kotlin/co/ke/jdfkajf/soko_garden/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package co.ke.jdfkajf.soko_garden
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
